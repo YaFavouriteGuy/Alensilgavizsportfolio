bd2285262cb67eb2a64a5c40cf54729d17756f00
YaFavouriteGuy <58549497+YaFavouriteGuy@users.noreply.github.com>
2020-04-03 13:49:17 +0300

    Add files via upload

---
+++ Alensilgavizsportfolio-master/app.js
@@ -0,0 +1,36 @@
+$(document).ready(function() { 
+
+    $("#body_container").css("display", "block");
+    $("#image_container").css("display", "none");
+    $("#form_content").css("display", "none");
+
+    $("#header").css("background", "white");
+    $("#header").css("width", "100%");
+    $("#header").css("height", "20%");
+
+    $("#body").css("background", "white");
+    $("#body").css("width", "100%");
+    $("#body").css("height", "75%");
+    
+    $("#footer").css("background", "grey");
+    $("#footer").css("width", "100%");
+    $("#footer").css("height", "5%");
+
+    $("#navigation_header").css("padding-top", "20px");
+    $("#navigation_header").css("padding-bottom", "20px");
+
+    $("#body_content").css("background", "yellow");
+
+    $("#personal_photo").css("height", "250px");
+    $("#personal_photo").css("width", "250px");
+    
+    $("#body_content").css("padding-top", "150px");
+    $(".magic-image").css("height", "350px")
+    $(".magic-image").css("width", "350px")
+
+    $("#image_content").css("padding-top", "100px");
+    $("#form_content").css("padding-top", "100px");
+    $var button = document.createElement("button");
+button.innerHTML = "Mani darbi";
+
+});
