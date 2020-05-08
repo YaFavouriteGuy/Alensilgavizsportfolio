e04630e199572edbafb4ce5f3ed53b0fd163842c
YaFavouriteGuy <58549497+YaFavouriteGuy@users.noreply.github.com>
2020-05-08 10:30:36 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -4,31 +4,57 @@ $(document).ready(function() {
     $("#image_container").css("display", "none");
     $("#form_content").css("display", "none");
 
-    $("#header").css("background", "black");
+    $("#header").css("background", "transparent");
     $("#header").css("width", "100%");
-    $("#header").css("height", "20%");
+    $("#header").css("height", "15%");
 
-    $("#body").css("background", "blue");
+    $("#body").css("background", "transparent");
     $("#body").css("width", "100%");
-    $("#body").css("height", "75%");
+    $("#body").css("height", "80%");
     
-    $("#footer").css("background", "red");
+    $("#footer").css("background", "grey");
     $("#footer").css("width", "100%");
     $("#footer").css("height", "5%");
 
     $("#navigation_header").css("padding-top", "20px");
     $("#navigation_header").css("padding-bottom", "20px");
 
-    $("#body_content").css("background", "yellow");
+    $("#navigation_buttons").css("height" , "20px")
+    $("#navigation_buttons").css("height" , "20px")
+    
+    $("#body_content").css("background", "transparent");
 
     $("#personal_photo").css("height", "250px");
-    $("#personal_photo").css("width", "250px");
+    $("#personal_photo").css("width", "1110px");
     
-    $("#body_content").css("padding-top", "150px");
-    $(".magic-image").css("height", "350px")
-    $(".magic-image").css("width", "350px")
+    $("#body_content").css("padding-top", "0px");
+    $(".magic-image").css("height", "380px")
+    $(".magic-image").css("width", "400px")
 
     $("#image_content").css("padding-top", "100px");
     $("#form_content").css("padding-top", "100px");
+   
+   $("#bruh1").click(function() {
+    $("#body_container").css("display", "block");
+    $("#image_container").css("display", "none");
+    $("#form_content").css("display", "none");
+    $("#main-body").css("background","url('balons.jpg')");
+   });
+
+   $("#bruh2").click(function() {
+    $("#body_container").css("display", "none");
+    $("#image_container").css("display", "block");
+    $("#form_content").css("display", "none");
+    $("#main-body").css("background","url('koki.jpg')");
+
+   });
+
+   $("#bruh3").click(function() {
+    $("#body_container").css("display", "none");
+    $("#image_container").css("display", "none");
+    $("#form_content").css("display", "block");
+    $("#main-body").css("background","url('plava.jpg')");
+   });
+
 
 });
