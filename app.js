3c8e70ffa59517551620f2f6932186ca0f7694e6
YaFavouriteGuy <58549497+YaFavouriteGuy@users.noreply.github.com>
2020-05-13 14:10:30 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
     $("#body_container").css("display", "block");
     $("#image_container").css("display", "none");
     $("#form_content").css("display", "none");
+    $("#row").css("background", "#ffffff")
 
     $("#header").css("background", "transparent");
     $("#header").css("width", "100%");
@@ -12,7 +13,7 @@ $(document).ready(function() {
     $("#body").css("width", "100%");
     $("#body").css("height", "80%");
     
-    $("#footer").css("background", "grey");
+    $("#footer").css("background", "DarkGoldenRod");
     $("#footer").css("width", "100%");
     $("#footer").css("height", "5%");
 
@@ -24,8 +25,8 @@ $(document).ready(function() {
     
     $("#body_content").css("background", "transparent");
 
-    $("#personal_photo").css("height", "250px");
-    $("#personal_photo").css("width", "1110px");
+    $("#personal_photo").css("height", "200px");
+    $("#personal_photo").css("width", "200px");
     
     $("#body_content").css("padding-top", "0px");
     $(".magic-image").css("height", "380px")
@@ -33,20 +34,33 @@ $(document).ready(function() {
 
     $("#image_content").css("padding-top", "100px");
     $("#form_content").css("padding-top", "100px");
+
+    $("#body").css("background", "transparent");
+    $("#body").css("width", "100%");
+    $("#body").css("height", "80%");
+ 
+    $("#main-body").css("background","url('balons.jpg')");
    
    $("#bruh1").click(function() {
     $("#body_container").css("display", "block");
     $("#image_container").css("display", "none");
     $("#form_content").css("display", "none");
     $("#main-body").css("background","url('balons.jpg')");
+    $("#footer").css("background", "DarkGoldenRod");
+    $(".poga1").css("background", "#ff9e00");
+    $(".poga2").css("background", "#ffb843");
+    $(".poga3").css("background", "#ffc86e");
    });
 
    $("#bruh2").click(function() {
     $("#body_container").css("display", "none");
     $("#image_container").css("display", "block");
     $("#form_content").css("display", "none");
-    $("#main-body").css("background","url('koki.jpg')");
-
+    $("#main-body").css("background","url('kokz.jpg')");
+    $("#footer").css("background", "OliveDrab");
+    $(".poga1").css("background", "#345D36");
+    $(".poga2").css("background", "#4B814D");
+    $(".poga3").css("background", "#60A263");
    });
 
    $("#bruh3").click(function() {
@@ -54,7 +68,10 @@ $(document).ready(function() {
     $("#image_container").css("display", "none");
     $("#form_content").css("display", "block");
     $("#main-body").css("background","url('plava.jpg')");
+    $("#footer").css("background", "Peru");
+    $(".poga1").css("background", "#BB8B36");
+    $(".poga2").css("background", "#DAA23F");
+    $(".poga3").css("background", "#F5B647");
    });
-
 
 });
